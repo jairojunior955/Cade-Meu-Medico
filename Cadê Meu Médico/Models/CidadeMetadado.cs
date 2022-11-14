d3fb74d323805a7d2e5e8f36b08a8b7171f2fe07
Jairo Santos jr <79456331+jairojunior955@users.noreply.github.com>
2022-11-14 14:12:46 -0300

    Add files via upload

---
+++ Cadê Meu Médico/Models/CidadeMetadado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CadeMeuMedico.Models
+{
+    [MetadataType(typeof(CidadeMetadado))]
+    public partial class cidades
+    {
+
+    }
+    public class CidadeMetadado
+    {
+        [Required(ErrorMessage = "Obrigatório Informar o nome da cidade")]
+        [StringLength(100,ErrorMessage = "O nome da cidade deve conter no máximo 100 caracteres")]
+        public string Nome { get; set; }
+    }
+}
---
+++ Cadê Meu Médico/Models/EspecialidadeMetadado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CadeMeuMedico.Models
+{
+    [MetadataType(typeof(EspecialidadeMetadado))]
+    public partial class Especialidade
+    {
+
+    }
+    public class EspecialidadeMetadado
+    {
+        [Required(ErrorMessage = "Obrigatório Informar a especialidade")]
+        [StringLength(80,ErrorMessage = "A especialidade deve conter no máximo 80 caracteres")]
+        public string Nome { get; set; }
+    }
+}
