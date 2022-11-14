62403a8f2c1b032649a17f02bdc73c6351eb3c59
Jairo Santos jr <79456331+jairojunior955@users.noreply.github.com>
2022-11-14 14:47:06 -0300

    Add files via upload

---
+++ Cadê Meu Médico/Repositorios/RepositorioUsuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CadeMeuMedico.Repositorios
+{
+    public class RepositorioUsuario
+    {
+        public static bool AutenticarUsuario(string login,string senha)
+        {
+            var SenhaCriptografada = Forms
+        }
+    }
+}
